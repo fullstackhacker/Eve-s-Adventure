abd1f2a3f53d448be15400e26fac8147e196f31d
shikon389 <siconninja@gmail.com>
2014-10-13 15:00:29 -0400

    JavaDoc filled out for all Item objects.

--- src/models/Item.java
+++ src/models/Item.java
@@ -1,5 +1,13 @@
 package models;
 
-public class Item extends EObject{
+/**
+ * The Item class extends EObject and acts as a super class for the Bamboo,
+ * Shrub, Tree and Wall objects. Methods defined here are methods that all sub
+ * Item object share.
+ * 
+ * @author Carmine Iannaccone
+ * 
+ */
+public class Item extends EObject {
 
 }
