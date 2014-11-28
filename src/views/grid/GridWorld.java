1ccbf11ff20db15a39357acfbebbfc560dc2c58b
Shikon <siconninja@gmail.com>
2014-11-28 04:18:44 -0500

    Refined GUI and added GridWorld

---
+++ src/views/grid/GridWorld.java
@@ -0,0 +1,59 @@
+package views.grid;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+
+public final class GridWorld extends GridPane {
+
+	private static GridWorld instant = null;
+
+	private GridWorld() {
+
+		this.setGridLinesVisible(true);
+
+		ColumnConstraints column1 = new ColumnConstraints();
+		column1.setPercentWidth(20);
+		ColumnConstraints column2 = new ColumnConstraints();
+		column2.setPercentWidth(20);
+		ColumnConstraints column3 = new ColumnConstraints();
+		column3.setPercentWidth(20);
+		ColumnConstraints column4 = new ColumnConstraints();
+		column4.setPercentWidth(20);
+		ColumnConstraints column5 = new ColumnConstraints();
+		column5.setPercentWidth(20);
+		this.getColumnConstraints().addAll(column1, column2, column3, column4,
+				column5);
+
+		RowConstraints row1 = new RowConstraints();
+		row1.setPercentHeight(10);
+		RowConstraints row2 = new RowConstraints();
+		row2.setPercentHeight(10);
+		RowConstraints row3 = new RowConstraints();
+		row3.setPercentHeight(10);
+		RowConstraints row4 = new RowConstraints();
+		row4.setPercentHeight(10);
+		RowConstraints row5 = new RowConstraints();
+		row5.setPercentHeight(10);
+		RowConstraints row6 = new RowConstraints();
+		row6.setPercentHeight(10);
+		RowConstraints row7 = new RowConstraints();
+		row7.setPercentHeight(10);
+		RowConstraints row8 = new RowConstraints();
+		row8.setPercentHeight(10);
+		RowConstraints row9 = new RowConstraints();
+		row9.setPercentHeight(10);
+		RowConstraints row10 = new RowConstraints();
+		row10.setPercentHeight(10);
+		this.getRowConstraints().addAll(row1, row2, row3, row4, row5, row6,
+				row7, row8, row9, row10);
+		
+		this.setPadding(new Insets(5, 5, 5, 5));
+
+	}
+
+	public static GridWorld getInstance() {
+		return (instant == null) ? new GridWorld() : instant;
+	}
+}
--- src/views/tips/ProTips.java
+++ src/views/tips/ProTips.java
@@ -2,6 +2,7 @@ package views.tips;
 
 import views.tabs.GameTabs;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -9,7 +10,7 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
-public final class ProTips extends HBox {
+public final class ProTips extends GridPane {
 
 	private static ProTips instant = null;
 
@@ -17,13 +18,25 @@ public final class ProTips extends HBox {
 		final Label LTips = new Label("Pro Tips! - ");
 		final Label TFTips = new Label(
 				"This is a long example message to see how far out this message will go. In the future I may need to make this have a text wrap. Who knows. Man, I really hope this is long enough. I mean, will our tips really be longer then this. Maaaybe the objectives...whatever.");
-		this.getChildren().addAll(LTips, TFTips);
+		//this.getChildren().addAll(LTips, TFTips);
 
-		LTips.setMinSize(100, 200);
-		TFTips.setMinSize(100, 200);
+		/*LTips.setMinSize(100, 200);
+		TFTips.setMinSize(100, 200);*/
+		
+		this.add(LTips, 0, 0);
+		this.add(TFTips, 1, 0);
+		
+		ColumnConstraints column1 = new ColumnConstraints();
+		column1.setPercentWidth(10);
+		ColumnConstraints column2 = new ColumnConstraints();
+		column2.setPercentWidth(90);
+		this.getColumnConstraints().addAll(column1, column2);
+		
 		GridPane.setHalignment(LTips, HPos.LEFT);
 		GridPane.setHalignment(TFTips, HPos.LEFT);
 		TFTips.setWrapText(true);
+		
+		this.setPadding(new Insets(5, 5, 5, 5));
 
 	}
 
