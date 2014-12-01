d312821509d82e471d32a84108af6f825e8f2170
MeganMurray <megansmurray@gmail.com>
2014-12-01 18:43:58 -0500

    added buttons for creatue / items, dont do anything yet

--- src/views/tabs/CreaturesTab.java
+++ src/views/tabs/CreaturesTab.java
@@ -1,5 +1,8 @@
 package views.tabs;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 final class CreaturesTab extends VBox{
@@ -7,6 +10,18 @@ final class CreaturesTab extends VBox{
 	private static final CreaturesTab INSTANCE = new CreaturesTab();
 	
 	private CreaturesTab(){
+		this.setSpacing(10);
+		this.setPadding(new Insets(0, 20, 10, 20));
+		this.setAlignment(Pos.CENTER);
+		
+		/* Making all the buttons */
+		final Button EVE = new Button("Eve");
+
+		/* Width */
+		EVE.setMaxWidth(GameTabs.GAMETABS_MAX_WIDTH);
+		
+		/* Add to Pane */
+		this.getChildren().addAll(EVE);
 		
 	}
 	
